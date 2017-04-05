65e6a59af1211ea1e9fc72911cc23b8a65fb454c
shikeyue <xinluqishi@126.com>
2017-04-05 17:27:30 +0800

    finishing the class UserService

--- app/src/main/java/com/xinluqishi/parkingcar/service/impl/UserService.java
+++ app/src/main/java/com/xinluqishi/parkingcar/service/impl/UserService.java
@@ -12,7 +12,7 @@ public class UserService implements IUserService{
 
 
     @Override
-    public void login(final String username, final String password, OnLoginListener loginListener) {
+    public void login(final String username, final String password, final OnLoginListener loginListener) {
         new Thread() {
             @Override
             public void run() {
@@ -25,7 +25,10 @@ public class UserService implements IUserService{
                 if ("shikeyue".equals(username) && "123".equals(password)) {
                     User user = new User();
                     user.setUsername(username);
-
+                    user.setPassword(password);
+                    loginListener.loginSuccess(user);
+                } else {
+                    loginListener.loginFailed();
                 }
             }
         }.start();
