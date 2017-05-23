02715ed341960af96893f6ae89cb40f7461b9ea9
shikeyue <xinluqishi@126.com>
2017-05-23 16:47:30 +0800

    Fixing the BRVAH framework importing

--- app/src/main/java/com/xinluqishi/parkingcar/adapter/HotCarTypeQuickAdapter.java
+++ app/src/main/java/com/xinluqishi/parkingcar/adapter/HotCarTypeQuickAdapter.java
@@ -1,8 +1,24 @@
 package com.xinluqishi.parkingcar.adapter;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.xinluqishi.parkingcar.R;
+import com.xinluqishi.parkingcar.model.entity.Car;
+
+import java.util.List;
+
 /**
  * Created by shikeyue on 17/5/17.
  */
 
-public class HotCarTypeQuickAdapter {
+public class HotCarTypeQuickAdapter extends BaseQuickAdapter<Car, BaseViewHolder> {
+
+    public HotCarTypeQuickAdapter(List<Car> data) {
+        super(R.layout.activity_car_type_list, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, Car item) {
+
+    }
 }
---
+++ app/src/main/java/com/xinluqishi/parkingcar/view/HotCarTypeListActivity.java
@@ -0,0 +1,30 @@
+package com.xinluqishi.parkingcar.view;
+
+import android.os.Bundle;
+
+import com.xinluqishi.parkingcar.ParkingCarActivity;
+import com.xinluqishi.parkingcar.adapter.HotCarTypeQuickAdapter;
+
+/**
+ * Created by shikeyue on 17/5/23.
+ */
+
+public class HotCarTypeListActivity extends ParkingCarActivity {
+
+
+    private HotCarTypeQuickAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initView();
+
+    }
+
+
+    private void initView() {
+
+    }
+
+}
